bf0d3afc0ed12fc22042dea0a750bb29fcffbe2b
prolisbet <lizatrushina96@gmail.com>
2024-07-20 22:26:35 +0300

    supress code through function and dictionaries

--- lesson-VD07-project/app/routes.py
+++ lesson-VD07-project/app/routes.py
@@ -49,39 +49,88 @@ def logout():
     return redirect(url_for('home'))
 
 
+def handle_form(form_class, form_action, success_message, password):
+    form = form_class(request.form)
+    if form.validate_on_submit():
+        if password:
+            if not current_user.check_password(form.current_password.data):
+                flash('Current password is incorrect', 'danger')
+                return form
+        form_action(form)
+        # db.session.commit()
+        # flash(success_message, 'success')
+        # return redirect(url_for('account'))
+        try:
+            db.session.commit()
+            flash(success_message, 'success')
+            return redirect(url_for('account'))
+        except Exception as e:
+            db.session.rollback()
+            flash(f'Error: {str(e)}', 'danger')
+            return form
+    else:
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash(f'Error in {getattr(form, field).label.text}: {error}', 'danger')
+    return form
+
+
 @app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
     form = None
+    form_starters = {
+        'change_name': ChangeNameForm,
+        'change_email': ChangeEmailForm,
+        'change_password': ChangePasswordForm
+    }
+    form_handlers = {
+        'new_username': (ChangeNameForm, lambda form: setattr(current_user, 'username', form.new_username.data),
+                         'Your username has been updated!', False),
+        'new_email': (ChangeEmailForm, lambda form: setattr(current_user, 'email', form.new_email.data),
+                      'Your email has been updated!', False),
+        'current_password': (ChangePasswordForm, lambda form: current_user.set_password(form.new_password.data),
+                             'Your password has been updated!', True)
+    }
     if request.method == 'POST':
-        if 'change_name' in request.form:
-            form = ChangeNameForm()
-        elif 'change_email' in request.form:
-            form = ChangeEmailForm()
-        elif 'change_password' in request.form:
-            form = ChangePasswordForm()
-        elif 'new_username' in request.form:
-            form = ChangeNameForm(request.form)
-            if form.validate_on_submit():
-                current_user.username = form.new_username.data
-                db.session.commit()
-                flash('Your username has been updated!', 'success')
-                return redirect(url_for('account'))
-        elif 'new_email' in request.form:
-            form = ChangeEmailForm(request.form)
-            if form.validate_on_submit():
-                current_user.email = form.new_email.data
-                db.session.commit()
-                flash('Your email has been updated!', 'success')
-                return redirect(url_for('account'))
-        elif 'current_password' in request.form:
-            form = ChangePasswordForm(request.form)
-            if form.validate_on_submit():
-                if current_user.check_password(form.current_password.data):
-                    current_user.set_password(form.new_password.data)
-                    db.session.commit()
-                    flash('Your password has been updated!', 'success')
-                    return redirect(url_for('account'))
-                else:
-                    flash('Current password is incorrect', 'danger')
+        for key, value in form_starters.items():
+            if key in request.form:
+                form = value()
+                return render_template('account.html', form=form)
+        for key, value in form_handlers.items():
+            if key in request.form:
+                form_class, form_action, success_message, password = value
+                form = handle_form(form_class, form_action, success_message, password)
     return render_template('account.html', form=form)
+
+    #     if 'change_name' in request.form:
+    #         form = ChangeNameForm()
+    #     elif 'change_email' in request.form:
+    #         form = ChangeEmailForm()
+    #     elif 'change_password' in request.form:
+    #         form = ChangePasswordForm()
+    #     elif 'new_username' in request.form:
+    #         form = ChangeNameForm(request.form)
+    #         if form.validate_on_submit():
+    #             current_user.username = form.new_username.data
+    #             db.session.commit()
+    #             flash('Your username has been updated!', 'success')
+    #             return redirect(url_for('account'))
+    #     elif 'new_email' in request.form:
+    #         form = ChangeEmailForm(request.form)
+    #         if form.validate_on_submit():
+    #             current_user.email = form.new_email.data
+    #             db.session.commit()
+    #             flash('Your email has been updated!', 'success')
+    #             return redirect(url_for('account'))
+    #     elif 'current_password' in request.form:
+    #         form = ChangePasswordForm(request.form)
+    #         if form.validate_on_submit():
+    #             if current_user.check_password(form.current_password.data):
+    #                 current_user.set_password(form.new_password.data)
+    #                 db.session.commit()
+    #                 flash('Your password has been updated!', 'success')
+    #                 return redirect(url_for('account'))
+    #             else:
+    #                 flash('Current password is incorrect', 'danger')
+    # return render_template('account.html', form=form)
